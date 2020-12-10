55ae057d3b0b6de2a23272852f3c77a0a640fd80
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-12-10 13:09:08 +0200

    Use Fuzzer now.

--- web-service/src/s3support/fuzz_test.go
+++ web-service/src/s3support/fuzz_test.go
@@ -1,11 +1,16 @@
 package s3support
 
 import (
+	"bytes"
 	"os"
 	"path"
 	"runtime"
 	"testing"
+
 	initialize "web-service/src/init/test_init"
+
+	fuzz "github.com/google/gofuzz"
+	guuid "github.com/google/uuid"
 )
 
 func TestMain(m *testing.M) {
@@ -21,5 +26,19 @@ func TestMain(m *testing.M) {
 }
 
 func TestFoo(t *testing.T) {
+	// Configre the Fuzzer, if needed
+	f := fuzz.New()
 
+	var raw_bytes []byte
+	f.Fuzz(&raw_bytes)
+
+	buffer := bytes.NewBuffer(raw_bytes)
+
+	var id guuid.UUID
+	f.Fuzz(&id)
+
+	var fileName string
+	f.Fuzz(&fileName)
+
+	StoreFileByUUID(id, buffer, fileName)
 }
